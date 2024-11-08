23abee2293f2fc4a42f89f11165f6e170e170bb3
ederbiason <ederbiason@hotmail.com>
2024-11-08 12:43:44 -0300

    feat: add cors config to enable cors of frontend url domain

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService)
 
   app.useGlobalPipes(new ValidationPipe())
+
+  const frontendOrigin = configService.get<string>('FRONTEND_DOMAIN')
+
+  app.enableCors({
+    origin: frontendOrigin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
 
   await app.listen(process.env.PORT ?? 3000);
 }
