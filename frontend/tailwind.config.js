5befcfc6b264ab427198c71c6b23e43bac405895
ederbiason <ederbiason@hotmail.com>
2025-01-14 16:07:43 -0300

    fix: change require to import tailwind animate

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import * as tailwindAnimate from "tailwindcss-animate"
 export default {
 	darkMode: ["class"],
 	content: [
@@ -92,6 +93,6 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindAnimate],
 }
 
